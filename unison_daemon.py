6c27c02ea008abdfbc232fe776ad48435ff486eb
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2018-01-07 12:02:24 -0800

    Add remote directory creation

--- unison_daemon.py
+++ unison_daemon.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os.path as osp
+import subprocess
 
 PREFERENCE_FILE_TEMPLATE = """\
 root = {local_root}
@@ -15,19 +16,33 @@ UNISON_DIRECTORY = osp.expanduser('~/.unison')
 
 LOCAL_BASE = osp.expanduser('~/workspace')
 REMOTE_BASE = '/mnt/d/workspace'
-REMOTE_HOST = 'surface'
 
 
-def create_preference_file(name, root):
+def create_remote_root(remote_host, root):
+    remote_cmd = 'mkdir {}'.format(osp.join(REMOTE_BASE, root))
+    subprocess.check_call(' '.join([
+        'ssh', remote_host,
+        '-C', '"{}"'.format(remote_cmd),
+    ]), shell=True)
+
+
+def create_preference_file(remote_host, name, root):
     filename = osp.join(UNISON_DIRECTORY, '{}.prf'.format(name))
     with open(filename, 'x') as f:
         f.write(PREFERENCE_FILE_TEMPLATE.format(
             local_root=osp.join(LOCAL_BASE, root),
             remote_root=osp.join(REMOTE_BASE, root),
-            remote_host=REMOTE_HOST,
+            remote_host=remote_host,
         ))
+
+
+REMOTE_HOST = 'surface'
 
 
 if __name__ == '__main__':
     import sys
-    create_preference_file(sys.argv[1], sys.argv[2])
+
+    name = sys.argv[1]
+    root = sys.argv[2]
+    create_remote_root(REMOTE_HOST, root)
+    create_preference_file(REMOTE_HOST, name, root)
